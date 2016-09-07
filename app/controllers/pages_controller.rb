cc9d7bf6595e6cfe415754306737f8611f6e577e
Joe Zhou <chn.challenger@gmail.com>
2016-09-07 12:28:07 +0100

    home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+  def home
+    @message = 'Welcome to One Maths!'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  controller :pages do
+    get :home
+    # get :about
+    # get :contact
+    # get :faq
+    # get :blog
+  end
+
 end
